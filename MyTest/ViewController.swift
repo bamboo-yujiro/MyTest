cf2f8a08d727b4aac255f74f299bda545aa27a63
bamboo-yujiro <ch11.naji@gmail.com>
2018-07-07 16:51:00 +0900

    first commit

--- MyTest/ViewController.swift
+++ MyTest/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import Alamofire
+import vfrReader
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ReaderViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let urlString: String = "http://www.pdf995.com/samples/pdf.pdf"
+        let destination = DownloadRequest.suggestedDownloadDestination()
+        Alamofire.download(urlString, to: destination).validate().responseData { response in
+            debugPrint(response)
+            guard
+                let pdfPath = response.destinationURL?.path,
+                let document = ReaderDocument(filePath: pdfPath, password: nil),
+                let readerVC = ReaderViewController(readerDocument: document) else {
+                return
+            }
+            readerVC.delegate = self
+            readerVC.modalPresentationStyle = .fullScreen
+            self.present(readerVC, animated: false, completion: nil)
+        }
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Podfile
@@ -0,0 +1,27 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MyTest' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MyTest
+  pod 'Alamofire', '4.7.2'
+  pod 'RxSwift', '4.2.0'
+  pod 'vfrReader', '2.8.6'
+
+  target 'MyTestTests' do
+    inherit! :search_paths
+    # Pods for testing
+    pod 'Alamofire', '4.7.2'
+    pod 'RxSwift', '4.2.0'
+  end
+
+  target 'MyTestUITests' do
+    inherit! :search_paths
+    # Pods for testing
+    pod 'Alamofire', '4.7.2'
+    pod 'RxSwift', '4.2.0'
+  end
+
+end
